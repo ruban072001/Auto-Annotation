9ddf9b43691641e8f20744f8d48cd78b1cc1f7d3
ruban072001 <rubansruban07@gmail.com>
2025-01-19 22:56:07 +0530

    first commit

---
+++ template_matching/sum_of_squared_diff.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+"""
+Template matching based on sum of squared difference
+original_image size = 1089, 1500, 3
+template_size = 60, 137, 3
+"""
+
+Original_img = cv2.imread(r"C:\Users\KIRUBA\Pictures\birds in the sky.jpg")
+Original_img = cv2.resize(Original_img, (0,0), fx=0.3, fy=0.3)
+template = cv2.imread(r"C:\Users\KIRUBA\Downloads\birds in the sky (1).jpg")
+template = cv2.resize(template, (0,0), fx=0.3, fy=0.3)
+
+ori_img_h, ori_img_w = Original_img.shape[:2]
+tem_h, tem_w = template.shape[:2]
+
+SSD_map = np.zeros((ori_img_h - tem_h, ori_img_w - tem_w))
+
+for i in range(ori_img_h - tem_h):
+    for j in range(ori_img_w - tem_w):
+        
+        roi = Original_img[i : i + tem_h, j : j + tem_w]
+        ssd = np.sum((roi - template) ** 2)
+        
+        SSD_map[i, j] = ssd
+        
+# min_val = np.min(SSD_map) ; print(min_val)
+# min_loc = np.unravel_index(np.argmin(SSD_map), SSD_map.shape) ; print(min_loc)
+threshold = 68500
+matches = np.where(SSD_map <= threshold)
+print(matches)
+for match in zip(matches[0], matches[1]):
+    top_left = match[1], match[0]
+    bottom_right = top_left[0] + tem_w, top_left[1] + tem_h
+
+    cv2.rectangle(Original_img, top_left, bottom_right, (0, 255, 0), 2)
+cv2.imshow('template', Original_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
